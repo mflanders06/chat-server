b44df0840efab86c919a255ee5a1900fae46d36d
Michael Flanders <mflanders06@gmail.com>
2021-03-31 22:20:46 -0600

    Looks great, but my id is not incrementing.

---
+++ server/controllers/messages_controller.js
@@ -0,0 +1,17 @@
+const messages = [];
+const id = 0;
+
+module.exports = {
+    read: (req, res) => {
+        res.status(200).send(messages);
+    },
+
+    create: (req, res) => {
+        const { text, time } = req.body;
+        const newMessage = {id, text, time};
+        messages.push(newMessage);
+        res.status(200).send(messages);
+        id++;
+    }
+
+}
